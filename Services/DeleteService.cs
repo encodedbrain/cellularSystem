4a0c95ee37e8a6d0f4d796eb54f8fd42bd1cfb72
marcoDmc <marcodamasceno07@gmail.com>
2023-10-16 16:09:37 -0300

    feat: added services

---
+++ Services/DeleteService.cs
@@ -0,0 +1,24 @@
+﻿using cellularSystem.Data;
+using cellularSystem.Schemas;
+
+namespace cellularSystem.Services;
+
+public class DeleteService
+{
+    public async Task<bool> Delete(DeleteSchema prop)
+    {
+        using (var context = new CellphonesDb())
+        {
+            var cellphone = context.Cellphones.FirstOrDefault(cel => cel.Model == prop.Model 
+            && cel.Brand == prop.Brand && cel.Identity ==  prop.Identity);
+
+
+            if (cellphone is null) return false;
+
+
+            context.Cellphones.Remove(cellphone);
+            await context.SaveChangesAsync();
+            return true;
+        }
+    }
+}
