8be53528cdb8672fdc25a9360367426b8f59f3d3
marcoDmc <marcodamasceno07@gmail.com>
2023-10-16 16:10:45 -0300

    feat: added schemas

---
+++ Schemas/DeleteSchema.cs
@@ -0,0 +1,8 @@
+﻿namespace cellularSystem.Schemas;
+
+public class DeleteSchema
+{
+    public string Model { get; set; }
+    public string Brand { get; set; }
+    public int Identity { get; set; }
+}
---
+++ Schemas/UpdateSchema.cs
@@ -0,0 +1,10 @@
+﻿namespace cellularSystem.Schemas;
+
+public class UpdateSchema
+{
+    public string Model { get; set; }
+    public string Brand { get; set; }
+    public int Identity { get; set; }
+    public string Field { get; set; }
+    public string Update { get; set; }
+}
