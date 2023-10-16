080ad5025c77f6e082fdbc6c95e568383280bbf6
marcoDmc <marcodamasceno07@gmail.com>
2023-10-16 12:40:20 -0300

    feat: added schemas

--- Schemas/CellphoneSchema.cs
+++ Schemas/CellphoneSchema.cs
@@ -2,5 +2,37 @@
 
 public class CellphoneSchema
 {
-    
+    public string Model {get;set;}
+    public string Brand { get; set; }
+    public string ReleaseYear { get; set; } 
+    public decimal PriceLaunch { get; set; } 
+    public string  Dimensions { get; set; } 
+    public string Weight { get; set; } 
+    public bool WaterProtection { get; set; }
+    public string  Processor  { get; set; } 
+    public string OperationalSystem { get; set; } 
+    public int InternalStorage { get; set; }
+    public int RamMemory { get; set; }
+    public string Type { get; set; }
+    public int Size { get; set; }
+    public int Resolution { get; set; }
+    public string Frequency { get; set; }
+    public string Desinty { get; set; }
+    public string Protection { get; set; }
+    public string Extras { get; set; }
+    public string CameraMain { get; set; }
+    public string CameraFront { get; set; }
+    public string Batery { get; set; }
+    public string Charger { get; set; }
+    public bool ThreeG { get; set; }
+    public bool FourG { get; set; }
+    public bool FiveG { get; set; }
+    public string Wifi { get; set; }
+    public string Bluetooth { get; set; }
+    public bool Gps { get; set; }
+    public bool Nfc { get; set; }
+    public string Sound { get; set; }
+    public bool InfraRed { get; set; }
+    public string Usb { get; set; }
+    public string Sensors { get; set; }
 }
--- Schemas/UpdateSchema.cs
+++ Schemas/UpdateSchema.cs
@@ -2,5 +2,9 @@
 
 public class UpdateSchema
 {
-    
+    public string Model { get; set; }
+    public string Brand { get; set; }
+    public int Identity { get; set; }
+    public string Field { get; set; }
+    public string Update { get; set; }
 }
